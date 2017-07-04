a7189beffb28680d9634e08e7e0354231a6e7830
abryson <abryson@DESKTOP-SUUCVAS>
2017-07-04 14:24:03 +0100

    Test

--- datavault-worker/src/test/java/org/datavaultplatform/worker/queue/ReceiverIntegrationTest.java
+++ datavault-worker/src/test/java/org/datavaultplatform/worker/queue/ReceiverIntegrationTest.java
@@ -20,7 +20,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = {
         "classpath:datavault-worker.xml"})
 public class ReceiverIntegrationTest {
-
 	
 	@Value("${tempDir}")
     private String tempDir;
